6311cf92690844fa214bef40baa85dc292ccb912
Anaël Delorme <87323681+anaelDelorme@users.noreply.github.com>
2024-01-16 08:25:58 +0000

    new change button

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -33,14 +33,12 @@ window.addEventListener('DOMContentLoaded', event => {
         const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
         const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
         if (menuToggleBars) {
-
-            menuToggleBars.classList.remove('fa-xmark');
-            menuToggleBars.classList.add('fa-bars');
+            menuToggleBars.classList.remove('fa-bars');
+            menuToggleBars.classList.add('fa-xmark');
         }
         if (menuToggleTimes) {
-
-            menuToggleTimes.classList.remove('fa-bars');
-                        menuToggleTimes.classList.add('fa-xmark');
+            menuToggleTimes.classList.remove('fa-xmark');
+            menuToggleTimes.classList.add('fa-bars');
         }
     }
 
